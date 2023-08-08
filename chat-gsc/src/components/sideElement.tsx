370a55552d2ce2514104d6777853fe87435322ab
Griffin Clark <56231413+griffinclark10@users.noreply.github.com>
2023-08-08 17:06:08 -0300

    progress on sidebar

--- chat-gsc/src/components/sideElement.tsx
+++ chat-gsc/src/components/sideElement.tsx
@@ -5,9 +5,12 @@ interface SidebarElementProps {
 
 const SidebarElement: React.FC<SidebarElementProps> = ({ text, href }) => {
     return (
-        <div className="bg-teal-950 w-100 pl-px pr-px">
-            <p>{text}</p>
-        </div>
+        <li>
+            <a href={href} className="flex items-center p-2 text-gray-100 rounded-lg dark:text-white hover:bg-teal-800 dark:hover:bg-gray-700 group">
+                <svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" className="h-4 w-4" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path></svg>
+                <span className="ml-3">{text}</span>
+            </a>
+        </li>
     );
 }
 
--- chat-gsc/src/components/sidebar.tsx
+++ chat-gsc/src/components/sidebar.tsx
@@ -2,16 +2,48 @@ import SidebarElement from "./sideElement";
 
 const Sidebar: React.FC = () => {
     return (
-        <aside className="bg-teal-950 w-52 p-4">
-            <h1 className="text-slate-100"> Sidebar </h1>
-            <ul className="text-slate-100">
-                <SidebarElement text="Home" href="/" />
-                <SidebarElement text="About Me" href="/about-me" />
-                <SidebarElement text="Projects" href="/projects" />
-                <SidebarElement text="Experience" href="/experience" />
+        <div className="flex flex-col w-64 h-screen px-3 py-4 overflow-y-auto bg-teal-900 dark:bg-gray-800">
+            <div className="mb-1 flex flex-row gap-2">
+                <a className="flex p-3 items-center gap-3 transition-colors duration-200 text-white cursor-pointer text-sm rounded-md border border-white/20 hover:bg-gray-500/10 h-11 flex-shrink-0 flex-grow"><svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" className="h-4 w-4" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg>New chat</a>
+                <span className="" data-state="closed">
+                    <a className="flex p-3 gap-3 transition-colors duration-200 text-white cursor-pointer text-sm rounded-md border border-white/20 hover:bg-gray-500/10 h-11 w-11 flex-shrink-0 items-center justify-center">
+                        <svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" className="h-4 w-4" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                            <rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect>
+                            <line x1="9" y1="3" x2="9" y2="21"></line>
+                        </svg>
+                    </a>
+                </span>
+            </div>
+            <div className="sticky top-0 z-[16]">
+                <h3 className="h-9 pb-2 pt-3 px-3 text-xs text-gray-500 font-medium text-ellipsis overflow-hidden break-all">Pages</h3>
+            </div>
+            <ul className="space-y-2 font-medium">
+                <SidebarElement text="Home" href="#" />
+                <SidebarElement text="About Me" href="#" />
+                <SidebarElement text="Experience" href="#" />
+                <SidebarElement text="Projects" href="#" />
             </ul>
-            
-        </aside>
+            <div className="flex-grow" />
+                <div className="mb-1 flex flex-row gap-2">
+                    <span className="" data-state="closed">
+                        <a className="flex p-3 gap-3 transition-colors duration-200 text-white cursor-pointer text-sm rounded-md border border-white/20 hover:bg-gray-500/10 h-11 w-11 flex-shrink-0 items-center justify-center">
+                        <svg aria-hidden="true" className="octicon octicon-mark-github" height="24" version="1.1" viewBox="0 0 16 16" width="24">
+                            <path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path>
+                        </svg>
+                        </a>
+                    </span>
+                    <span className="" data-state="closed">
+                        <a className="flex p-3 gap-3 transition-colors duration-200 text-white cursor-pointer text-sm rounded-md border border-white/20 hover:bg-gray-500/10 h-11 w-11 flex-shrink-0 items-center justify-center">
+                            <svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" className="h-4 w-4" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                                <rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect>
+                                <line x1="9" y1="3" x2="9" y2="21"></line>
+                            </svg>
+                        </a>
+                    </span>
+
+                </div>
+                
+            </div>
     );
 }
 
