39cfcc7aee740acd6a3cd549c52b6f99647197c5
Griffin Clark <56231413+griffinclark10@users.noreply.github.com>
2023-08-08 17:55:46 -0300

    finished sidebar ui

--- chat-gsc/src/components/sidebar.tsx
+++ chat-gsc/src/components/sidebar.tsx
@@ -1,4 +1,7 @@
 import SidebarElement from "./sideElement";
+import { faGithub, faLinkedin } from "../../node_modules/@fortawesome/free-brands-svg-icons/index";
+import { faEnvelope, faFilePdf } from "../../node_modules/@fortawesome/free-solid-svg-icons/index";
+import { FontAwesomeIcon } from "../../node_modules/@fortawesome/react-fontawesome/index";
 
 const Sidebar: React.FC = () => {
     return (
@@ -6,7 +9,7 @@ const Sidebar: React.FC = () => {
             <div className="mb-1 flex flex-row gap-2">
                 <a className="flex p-3 items-center gap-3 transition-colors duration-200 text-white cursor-pointer text-sm rounded-md border border-white/20 hover:bg-gray-500/10 h-11 flex-shrink-0 flex-grow"><svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" className="h-4 w-4" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg>New chat</a>
                 <span className="" data-state="closed">
-                    <a className="flex p-3 gap-3 transition-colors duration-200 text-white cursor-pointer text-sm rounded-md border border-white/20 hover:bg-gray-500/10 h-11 w-11 flex-shrink-0 items-center justify-center">
+                    <a className="flex p-3 gap-3 transition-colors duration-200 text-white cursor-pointer rounded-md border border-white/20 hover:bg-gray-500/10 h-11 w-11 flex-shrink-0 items-center justify-center">
                         <svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" className="h-4 w-4" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
                             <rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect>
                             <line x1="9" y1="3" x2="9" y2="21"></line>
@@ -24,26 +27,29 @@ const Sidebar: React.FC = () => {
                 <SidebarElement text="Projects" href="#" />
             </ul>
             <div className="flex-grow" />
-                <div className="mb-1 flex flex-row gap-2">
-                    <span className="" data-state="closed">
-                        <a className="flex p-3 gap-3 transition-colors duration-200 text-white cursor-pointer text-sm rounded-md border border-white/20 hover:bg-gray-500/10 h-11 w-11 flex-shrink-0 items-center justify-center">
-                        <svg aria-hidden="true" className="octicon octicon-mark-github" height="24" version="1.1" viewBox="0 0 16 16" width="24">
-                            <path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path>
-                        </svg>
+            <div className="mb-1 flex flex-row gap-3 justify-center">
+                <span className="" data-state="closed">
+                    <a href="https://github.com/griffinclark10" className="flex p-3 gap-3 transition-colors duration-200 text-white cursor-pointer text-sm rounded-md border border-white/20 hover:bg-gray-500/10 h-11 w-11 flex-shrink-0 items-center justify-center">
+                        <FontAwesomeIcon icon={faGithub}/>
+                    </a>
+                </span>
+                <span className="" data-state="closed">
+                    <a href="https://www.linkedin.com/in/griffin-clark-a02513146/" className="flex p-3 gap-3 transition-colors duration-200 text-white cursor-pointer text-sm rounded-md border border-white/20 hover:bg-gray-500/10 h-11 w-11 flex-shrink-0 items-center justify-center">
+                        <FontAwesomeIcon icon={faLinkedin}/>
+                    </a>
+                </span>
+                <span className="" data-state="closed">
+                        <a href="mailto:griffinclark10@gmail.com" className="flex p-3 gap-3 transition-colors duration-200 text-white cursor-pointer text-sm rounded-md border border-white/20 hover:bg-gray-500/10 h-11 w-11 flex-shrink-0 items-center justify-center">
+                        <FontAwesomeIcon icon={faEnvelope}/>
                         </a>
-                    </span>
-                    <span className="" data-state="closed">
-                        <a className="flex p-3 gap-3 transition-colors duration-200 text-white cursor-pointer text-sm rounded-md border border-white/20 hover:bg-gray-500/10 h-11 w-11 flex-shrink-0 items-center justify-center">
-                            <svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" className="h-4 w-4" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
-                                <rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect>
-                                <line x1="9" y1="3" x2="9" y2="21"></line>
-                            </svg>
-                        </a>
-                    </span>
-
-                </div>
-                
-            </div>
+                </span>
+                <span className="" data-state="closed">
+                    <a href="#" className="flex p-3 gap-4 transition-colors duration-200 text-white cursor-pointer text-sm rounded-md border border-white/20 hover:bg-gray-500/10 h-11 w-11 flex-shrink-0 items-center justify-center">
+                        <FontAwesomeIcon icon={faFilePdf}/>
+                    </a>
+                </span>
+            </div>  
+        </div>
     );
 }
 
