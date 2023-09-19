d8ebc247e10eaa42f6057a3fc592ef5261c29e67
Griffin Clark <56231413+griffinclark10@users.noreply.github.com>
2023-09-19 12:58:38 -0700

    animation bug reprocussions from layout

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -5,9 +5,14 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { MouseEventHandler, useState } from "react";
 import Button from "./Button";
 
-const Sidebar = (props: { onClick: MouseEventHandler<HTMLAnchorElement>; hidden: boolean; isMobile: boolean }) => {
+const Sidebar = ({ onClick, hidden, isMobile, toggle }: {
+    onClick: MouseEventHandler<HTMLAnchorElement>;
+    hidden: boolean;
+    isMobile: boolean;
+    toggle: MouseEventHandler<HTMLSpanElement>;
+}) => {
     let buttonHeight = 11;
-    if (props.isMobile) {
+    if (isMobile) {
         buttonHeight = 18;
     }
 
@@ -20,7 +25,7 @@ const Sidebar = (props: { onClick: MouseEventHandler<HTMLAnchorElement>; hidden:
     };
 
     return (
-        <div className={`flex flex-col ${props.hidden ? 'w-0 overflow-auto' : 'w-full md:w-[300px] px-3 py-4'} md:z-0 h-screen md:overflow-y-auto bg-teal-900 dark:bg-gray-800 transition-all duration-1000`}>
+        <div className={`flex flex-col ${hidden ? 'w-0 overflow-auto' : 'w-full md:w-[300px] px-3 py-4'} md:z-0 h-screen md:overflow-y-auto bg-teal-900 dark:bg-gray-800 transition-all duration-1000`}>
             <div className="mb-1 flex flex-row gap-2">
                 <a className="flex p-3 items-center gap-3 transition-colors duration-200 text-white cursor-pointer text-sm rounded-md border border-white/20 hover:bg-gray-500/10 h-16 md:h-11 flex-shrink-0 flex-grow" onClick={handleAlert}><svg stroke="currentColor" fill="none" strokeWidth="2" viewBox="0 0 24 24" strokeLinecap="round" strokeLinejoin="round" className="h-4 w-4" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg>New chat</a>
                 {showAlert && (
@@ -29,7 +34,7 @@ const Sidebar = (props: { onClick: MouseEventHandler<HTMLAnchorElement>; hidden:
                     </div>
                 )}
                 <span className="" data-state="closed">
-                    <a className="flex p-3 gap-3 transition-colors duration-200 text-white cursor-pointer rounded-md border border-white/20 hover:bg-gray-500/10 h-16 md:h-11 w-16 md:w-11 flex-shrink-0 items-center justify-center" onClick={props.onClick}>
+                    <a className="flex p-3 gap-3 transition-colors duration-200 text-white cursor-pointer rounded-md border border-white/20 hover:bg-gray-500/10 h-16 md:h-11 w-16 md:w-11 flex-shrink-0 items-center justify-center" onClick={onClick}>
                         <svg stroke="currentColor" fill="none" strokeWidth="2" viewBox="0 0 24 24" strokeLinecap="round" strokeLinejoin="round" className="h-4 w-4" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
                             <rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect>
                             <line x1="9" y1="3" x2="9" y2="21"></line>
@@ -41,10 +46,10 @@ const Sidebar = (props: { onClick: MouseEventHandler<HTMLAnchorElement>; hidden:
                 <h3 className="h-9 pb-2 pt-3 px-3 text-xs text-gray-500 font-medium text-ellipsis overflow-hidden break-all">Pages</h3>
             </div>
             <ul className="space-y-2 font-medium">
-                <SidebarElement text="Home" href="/" />
-                <SidebarElement text="About Me" href="/about" />
-                <SidebarElement text="Experience" href="/experience" />
-                <SidebarElement text="Projects" href="/projects" />
+                <SidebarElement text="Home" href="/" toggle={toggle} isMobile={isMobile} />
+                <SidebarElement text="About Me" href="/about" toggle={toggle} isMobile={isMobile}/>
+                <SidebarElement text="Experience" href="/experience" toggle={toggle} isMobile={isMobile}/>
+                <SidebarElement text="Projects" href="/projects" toggle={toggle} isMobile={isMobile}/>
             </ul>
             <div className="flex-grow" />
             <div className="mb-1 flex flex-row gap-4 md:gap-2 justify-center">
--- src/components/SidebarElement.tsx
+++ src/components/SidebarElement.tsx
@@ -2,14 +2,19 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useRouter } from "next/router";
-import { useState } from "react";
+import { MouseEventHandler, useState } from "react";
 
-const SidebarElement = ({ text, href }: { text: string;  href: string}) => {
+const SidebarElement = ({ text, href, toggle, isMobile }: {
+    text: string;
+    href: string;
+    toggle: MouseEventHandler<HTMLSpanElement>;
+    isMobile: boolean;
+}) => {
     const path = usePathname();
     const [isHovering, setIsHovering] = useState(false);
     return (
         <li>
-            <Link href={href} className={`flex h-16 md:h-11 items-center p-2 text-gray-100 rounded-lg dark:text-white hover:bg-teal-800 ${path == href ? "bg-teal-800" : null} dark:hover:bg-gray-700 group`} onMouseEnter={() => setIsHovering(true)} onMouseOut={() => setIsHovering(false)}>
+            <Link href={href} className={`flex h-16 md:h-11 items-center p-2 text-gray-100 rounded-lg dark:text-white hover:bg-teal-800 ${path == href ? "bg-teal-800" : null} dark:hover:bg-gray-700 group`} onMouseEnter={() => setIsHovering(true)} onMouseOut={() => setIsHovering(false)} onClick={isMobile ? toggle : () => {}}>
                 <svg stroke="currentColor" fill="none" strokeWidth="2" viewBox="0 0 24 24" strokeLinecap="round" strokeLinejoin="round" className={`h-4 w-4 ${isHovering && "animate-longWiggle"}`} height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path></svg>
                 <span className="ml-3">{text}</span>
             </Link>
