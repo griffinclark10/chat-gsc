42c7807faf7900843822ba6214c247594cf92d78
Griffin Clark <56231413+griffinclark10@users.noreply.github.com>
2023-09-20 18:13:36 -0700

    semi fix to hidden mail popup, added pdfs

--- src/components/HeaderButton.tsx
+++ src/components/HeaderButton.tsx
@@ -9,10 +9,10 @@ const HeaderButton = ({ icon, openBars, animations, href}: {
 }) => {
 
     return (
-        <a className={`flex p-3 gap-3 transition-colors duration-200 cursor-pointer border rounded-md border-yellow-700 text-yellow-700 h-11 w-11 flex-shrink-0 items-center justify-center ${(openBars ? animations[0] : animations[1])}`} href={href}>
+        <a className={`flex p-3 gap-3 transition-colors duration-200 cursor-pointer border rounded-md border-yellow-700 text-yellow-700 h-11 w-11 flex-shrink-0 items-center justify-center ${(openBars ? `${animations[0]}` : animations[1])}`} href={openBars ? href : "#"}>
                 <FontAwesomeIcon 
                     icon={icon}
-                    className={`absolute transition-all `}
+                    className={`absolute transition-all`}
                 />
             </a>
     )
